74525c84c9df5504f5c27027fed32bb43c90b002
Dhanush-Kalvium <marella.reddy.s69@kalvium.community>
2025-02-21 09:03:25 +0530

    commited

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,7 @@
-// write button card here
+import "./button.css";
+
+const ViewProductButton = () => {
+  return <button className="view-product-button">View Product</button>;
+};
+
+export default ViewProductButton;
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,20 @@
 // write product card here
+import "./product.css";
+import ViewProductButton from "./button";
+import image from '/image.png'
+const productcard = () => {
+  const productImage = image;
+  const productName = "Apple Watch Series 7";
+  const price = "$399.99";
+
+  return (
+    <div className="product-card">
+      <img src={productImage} alt={productName} className="product-image" />
+      <h2 className="product-name">{productName}</h2>
+      <p className="price">{price}</p>
+      <ViewProductButton />
+    </div>
+  );
+};
+
+export default productcard;
